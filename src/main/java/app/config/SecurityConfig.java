9a579c13e922d641df2b258e208dc365836ccd5d
eliaslamanna <eliaslamanna@hotmail.com>
2024-04-03 02:58:55 -0300

    cors fix

--- src/main/java/app/config/SecurityConfig.java
+++ src/main/java/app/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,7 +24,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                         .anyRequest().permitAll()
                 )
-                .cors().and() // Enable CORS support
+                .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource())
+                ) // Enable CORS support
                 .csrf().disable()
                 .httpBasic().disable(); // Customize HTTP Basic authentication
 
@@ -27,4 +34,15 @@ public class SecurityConfig {
     }
     // Add your user authentication and other configurations as needed...
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://660cecff21bb390008f1a9ab--wigglypets.netlify.app")); // Add your front-end's URL
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
