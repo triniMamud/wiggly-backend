b904cfb55767db02ce7ce3c73c57f2bb63061c7e
eliaslamanna <eliaslamanna@hotmail.com>
2024-04-03 00:51:02 -0300

    deploy test

--- src/main/java/app/StartApp.java
+++ src/main/java/app/StartApp.java
@@ -3,9 +3,10 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityFilterAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityFilterAutoConfiguration.class, DataSourceAutoConfiguration.class})
 //@EntityScan("app.model.entity")
 public class StartApp {
     public static void main(String[] args) {
